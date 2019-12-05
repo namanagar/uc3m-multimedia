6665c629f418a778c183eb098af22bc9c3d579fc
justinspar <justinrspar@gmail.com>
2019-12-05 23:22:19 +0100

    scraping almost done

--- proj_2/Project2/src/main/java/finalLabMaven/Project2/ProjectMethods.java
+++ proj_2/Project2/src/main/java/finalLabMaven/Project2/ProjectMethods.java
@@ -1,5 +1,17 @@
 package finalLabMaven.Project2;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,12 +20,11 @@ import org.jsoup.select.Elements;
 
 public class ProjectMethods {
 	
-	public static final String URL_WIKIE = "https://www.imdb.com/title/tt0111161";
+	public static final String URL_WIKIE = "https://www.imdb.com/title/tt0010071/";
 	
-	public static void getTitle(String url) throws IOException{
+	public static void getTitle(String url) throws IOException{ // works
 		Document doc = Jsoup.connect(url).get();
 		System.out.println(url + ":\t" + doc.title());
-		System.out.println();
 	}
 	
 	public static void getHRef(String url) throws IOException{
@@ -24,13 +35,86 @@ public class ProjectMethods {
 		for (Element elem:lst) {
 			System.out.println("\t:" + elem.text());
 		}
-		
-		System.out.println();
 	}
 	
-	public static void main(String[] args) throws IOException {
-		getTitle(URL_WIKIE);
-		getHRef(URL_WIKIE);
+	public static void getYear(String url) throws IOException{
+		Document doc = Jsoup.connect(url).get();
+		Elements lst = doc.select("a[href]");
+		
+		System.out.println("\t:" + lst.get(84).text());
 	}
-
+	
+	
+	public static void getH4(String url) throws IOException{
+		Document doc = Jsoup.connect(url).get();
+		System.out.println("H4 from:\t" + doc.title());
+		
+		Elements lst = doc.select("h4");
+		for(Element elem:lst) {
+			System.out.println("\t:" + elem.childNode(0).toString());
+		}
+	}
+	
+	public static void getSummary(String url) throws IOException{ // summary, works
+		Document doc = Jsoup.connect(url).get();
+		Elements lst = doc.select("div.summary_text");
+		
+		for (Element elem:lst) {
+			System.out.println("\t:" + elem.text());
+		}
+	}
+	
+	public static void getDirector(String url) throws IOException{ // works
+		Document doc = Jsoup.connect(url).get();
+		Elements lst = doc.select("div.credit_summary_item"); //first one for director
+		
+		System.out.println(lst.get(0).text());
+	}
+	
+	public static void getKeywords(String url) throws IOException{ // works
+		Document doc = Jsoup.connect(url).get();
+		Elements lst = doc.select("div.see-more.inline.canwrap");
+		
+		System.out.println("\t:" + lst.get(0).text());
+	}
+	
+	public static void getGenres(String url) throws IOException{ // works
+		Document doc = Jsoup.connect(url).get();
+		Elements lst = doc.select("div.see-more.inline.canwrap");
+		
+		System.out.println(lst.get(1).text());
+	}
+	
+	public static void getStoryline(String url) throws IOException{ // works
+		Document doc = Jsoup.connect(url).get();
+		Elements lst = doc.select("div.inline.canwrap");
+	
+		System.out.println("\t:" + lst.get(0).text());
+	}
+	
+	public static void getCountry(String url) throws IOException{ // summary, works
+		Document doc = Jsoup.connect(url).get();
+		Elements lst = doc.select("div.txt-block");
+		
+		System.out.println(lst.get(3).text());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new FileReader("/Users/justinspar/Desktop/uc3m-multimedia/proj_2/Project2/src/main/java/finalLabMaven/Project2/movies.txt"));
+		
+		String line;
+		
+		while((line = in.readLine()) != null)
+		{
+		   getTitle(line);
+		   getDirector(line);
+		   getSummary(line);
+		   getStoryline(line);
+		   getKeywords(line);
+		   getGenres(line);
+		   getYear(line);
+		   getCountry(line);
+		   System.out.println();
+		}
+	}
 }
